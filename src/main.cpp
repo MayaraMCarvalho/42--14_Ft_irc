1ba7461afbafc4da478a6840fd94fc08e4faec42
macarval <macarval@student.42sp.org.br>
2024-06-07 12:17:59 -0300

    [ft_irc] refactor: testing the main

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@
 /*   By: macarval <macarval@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 16:51:56 by macarval          #+#    #+#             */
-/*   Updated: 2024/06/07 11:29:46 by macarval         ###   ########.fr       */
+/*   Updated: 2024/06/07 12:16:47 by macarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#include <iostream>
 
 int main(int argc, char const*argv[])
 {
